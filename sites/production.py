3e1385846e44d9d5f0f0565ee0da849d5ec63c0c
zhangmiao <zhangmiao@localhost.localdomain>
2015-05-13 15:24:43 +0800

    fixed utf8mb4 setting

--- sites/production.py
+++ sites/production.py
@@ -24,7 +24,8 @@ DATABASES = {
         'PORT': '3306', # Set to empty string for default.
         'NAME': 'yanqi',
         'OPTIONS': {
-            'init_command': 'SET storage_engine=INNODB,character_set_connection=utf8,collation_connection=utf8_unicode_ci'
+            'init_command': 'character_set_connection=utf8,collation_connection=utf8_unicode_ci',
+            'charset': 'utf8mb4'
         }
     }
 }
